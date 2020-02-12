08cb26d169ca9991c96a3915d79335a718829d82
HGPMAC 142 <rahsaan.jones@hgs.hiddengeniusproject.org>
2020-02-11 21:46:36 -0800

    add index and show pages actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,12 @@
 class MessagesController < ApplicationController
 	
 	def index
+		@messages = Message.all.order("created_at DESC")
 	end 
+
+	def show
+		@message = Message.find(params[:id])
+	end
 
 	def new
 		@message = Message.new
