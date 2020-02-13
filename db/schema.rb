793b59fb825c013e7bd33da504061588b7c51f60
HGPMAC 142 <rahsaan.jones@hgs.hiddengeniusproject.org>
2020-02-12 17:39:56 -0800

    create comments modle conroller and view files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_063814) do
+ActiveRecord::Schema.define(version: 2020_02_12_151855) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "message_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["message_id"], name: "index_comments_on_message_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
