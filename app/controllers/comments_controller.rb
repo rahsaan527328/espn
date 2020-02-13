5973896d3c97b52da6de976e9ba9d3b925c62b4c
HGPMAC 142 <rahsaan.jones@hgs.hiddengeniusproject.org>
2020-02-12 21:37:56 -0800

    Add styling and authentication to controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
-
+	before_action :authenticate_user!
+	before_action :find_message, only: [:create, :edit, :update, :destroy]
+	before_action :find_comment, only: [:edit, :update, :destroy]
 	def create
-		@message = Message.find(params[:message_id])
 		@comment = @message.comments.create(comment_params)
 		@comment.user_id = current_user.id
 
@@ -12,11 +13,36 @@ class CommentsController < ApplicationController
 		end 
 	end
 
+	def edit
+	end 
+
+	def update
+		if @comment.update(comment_params)
+			redirect_to message_path(@message)
+		else 
+			redner 'edit'
+		end 
+	end
+
+	def destroy
+		@comment.destroy
+		redirect_to message_path(@message)
+	end
+
+
 
 	private
 
 	def comment_params
 		params.require(:comment).permit(:content)
 
+	end
+
+	def find_message
+		@message = Message.find(params[:message_id])
+	end
+
+	def find_comment
+		@comment = @message.comments.find(params[:id])
 	end
 end
